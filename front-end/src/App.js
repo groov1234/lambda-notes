8b2ae4e88c53f230040c853032ec19d2616edc5c
groov1234 <c.groover@outlook.com>
2018-03-22 16:30:54 -0400

    building new front-end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/navbar';
+// import Greeting from './components/greeting';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Test from './components/test';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Route exact path="/" component={NavBar} />
+                    <Route exact path="/signup" component={Test} />
+                    <Route exact path="/signin" component={Test} />
+                    <Route exact path="/notes" component={Test} />
+                    <Route exact path="/users" component={Test} />
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
