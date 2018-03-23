5466b6867a7c7ab24b80b7485a39e332647bf89c
groov1234 <c.groover@outlook.com>
2018-03-22 22:56:18 -0400

    front-end added

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './Components/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './Reducers';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { HashRouter as Router } from 'react-router-dom';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+);
